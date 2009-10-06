5a0d9ecb601d9c7f6cf11364a83e58cba435bfc0
Rick Bradley <rick@rickbradley.com>
2009-10-06 18:10:11 +0530

    basic validity checking for an XML file

--- lib/yahoo_xml_parser.rb
+++ lib/yahoo_xml_parser.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 class YahooXMLParser
   def initialize(options)
     @verbose = options[:verbose]
@@ -12,7 +14,25 @@ class YahooXMLParser
   end
   
   def transform_xml
-    doc = xml
-    raise "XML document is not valid.  [#{doc}]" unless valid_xml?(doc)
+    doc = parsed_xml
+    raise "XML document is not valid:\n  [#{doc}]" unless valid_xml?(doc)
+  end
+  
+  def valid_xml?(parsed)
+    return false if     parsed.xpath('/Catalog').empty?
+    return false unless parsed.xpath('/Catalog').first['StoreID']
+    return false unless parsed.xpath('/Catalog').first['StoreName']
+
+    items = parsed.xpath('/Catalog/Item')
+    item_ids = items.collect {|item| item['ID'] }
+
+    return false if items.empty?
+    return false if items.any? {|item| item['ID'].nil? }
+    return false if item_ids.size != item_ids.uniq.size  # detect duplicate item id's
+
+    # ensure that all items have item fields
+    return false if items.any? {|item| item.xpath('ItemField').empty? }
+        
+    true
   end
 end
--- spec/yahoo_xml_parser_spec.rb
+++ spec/yahoo_xml_parser_spec.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[spec_helper]))
 require 'yahoo_xml_parser'
+require 'nokogiri'
 
 describe 'Yahoo XML Parser' do
   describe 'when initializing' do
@@ -69,7 +70,7 @@ describe 'Yahoo XML Parser' do
     before do
       @parser = YahooXMLParser.new({})
       @xml = 'sample XML document'
-      @parser.stub!(:xml).and_return(@xml)
+      @parser.stub!(:parsed_xml).and_return(@xml)
       @parser.stub!(:valid_xml?).with(@xml).and_return(true)
     end
     
@@ -81,13 +82,13 @@ describe 'Yahoo XML Parser' do
       lambda { @parser.transform_xml(:foo) }.should.raise(ArgumentError)
     end
     
-    it 'should fail if the XML document cannot be retrieved' do
-      @parser.stub!(:xml).and_raise(RuntimeError)
+    it 'should fail if the parsed XML document cannot be retrieved' do
+      @parser.stub!(:parsed_xml).and_raise(RuntimeError)
       lambda { @parser.transform_xml }.should.raise(RuntimeError)
     end
     
     it 'should fail if the XML document is not a valid Yahoo Store XML dump' do
-      @parser.stub!(:valid_xml?).with(@xml).and_return(false)
+      @parser.stub!(:valid_xml?).and_return(false)
       lambda { @parser.transform_xml }.should.raise(RuntimeError)
     end
     
@@ -98,11 +99,68 @@ describe 'Yahoo XML Parser' do
     
   end
   
-  describe 'XML document' do
+  describe 'parsed XML document' do
     
   end
   
   describe 'when validating an XML document' do
+    def parse(xml)
+      Nokogiri::XML.parse(xml)
+    end
     
+    before do
+      @parser = YahooXMLParser.new({})
+      @xml = File.read(File.join(File.dirname(__FILE__), *%w[.. fixtures catalog.xml]))
+      @parsed = parse(@xml)
+    end
+    
+    it 'should accept a parsed XML document' do
+      lambda { @parser.valid_xml?(@parsed) }.should.not.raise(ArgumentError)
+    end
+    
+    it 'should require a parsed XML document' do
+      lambda { @parser.valid_xml? }.should.raise(ArgumentError)
+    end
+    
+    it 'should consider a valid parsed Yahoo Store XML dump to be valid' do
+      @parser.valid_xml?(@parsed).should.be.true
+    end
+
+    it 'should require the XML root element to be a catalog element' do
+      @parsed.xpath('/Catalog').first.name = 'foo'
+      @parser.valid_xml?(@parsed).should.be.false
+    end
+    
+    it 'should require the XML root element to specify a store id' do
+      @parsed.xpath('/Catalog').first.delete('StoreID')
+      @parser.valid_xml?(@parsed).should.be.false      
+    end
+    
+    it 'should require the XML root element to specify a store name' do
+      @parsed.xpath('/Catalog').first.delete('StoreName')
+      @parser.valid_xml?(@parsed).should.be.false
+    end
+    
+    it 'should require the XML document to describe some set of items' do
+      @parsed.xpath('/Catalog/Item').each {|item| item.name = 'NonItem' }
+      @parser.valid_xml?(@parsed).should.be.false      
+    end
+    
+    it 'should require all items to have an item ID' do
+      @parsed.xpath('/Catalog/Item').last.delete('ID')
+      @parser.valid_xml?(@parsed).should.be.false      
+    end
+
+    it 'should require all items to have unique item IDs' do
+      items = @parsed.xpath('/Catalog/Item')
+      items.last['ID'] = items.first['ID']
+      @parser.valid_xml?(@parsed).should.be.false      
+    end
+    
+    it 'should have item fields for all items' do
+      item = @parsed.xpath('/Catalog/Item').last
+      item.xpath('ItemField').each {|item_field| item_field.name = 'Foo' }
+      @parser.valid_xml?(@parsed).should.be.false      
+    end
   end
 end
